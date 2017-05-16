c439fcf5a57fdf2ed5c535101b9fdbf2f468f52d
furkansahin <sahinffurkan@gmail.com>
2017-05-16 21:26:50 +0300

    working on new node arrival

--- Server.py
+++ Server.py
@@ -18,15 +18,21 @@ class Server:
             request = "incoming request"
             json_request = json.loads(request)
             ip = "127.0.0.1"
-            port = "2222"
-            hash = self.calculate_hash(ip + port)
-            self.send_response(ip, port, hash)
+            key = self.calculate_hash(ip)
+            self.send_response(ip, key)
+
+            for _ip in self.nodes.keys():
+                self.send_new_node(_ip, ip, key)
 
     def calculate_hash(self, definition):
         hex_hash = hashlib.sha1(definition).hexdigest()
         key = int(hex_hash, 16) % 2 ** self.system_m
         return key
 
-    def send_response(self, ip, port, hash):
-        message = json.dumps({"ip": ip, "id": hash, "idDictionary": self.nodes, "m": self.system_m})
+    def send_response(self, ip, key):
+        message = json.dumps({"ip": ip, "id": key, "idDictionary": self.nodes, "m": self.system_m})
         # send(message, ip, port)
+
+    def send_new_node(self, ip_to_sent, ip_to_be_sent, key):
+        message = json.dumps({"ip": ip_to_be_sent, "id": key})
+        # send(message, ip_to_sent)
